f23ad7d68fd6e14884151934c80ea95ac18abfe6
Paul Ogolla <paulotieno2@gmail.com>
2024-08-27 10:28:24 +0300

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" 10. Change school topics """
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    return mongo_collection.update
