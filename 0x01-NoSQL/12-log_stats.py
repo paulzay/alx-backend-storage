9f42614b63fd1845af1f2fd5913b6de1e4da597c
Paul Ogolla <paulotieno2@gmail.com>
2024-08-27 14:38:07 +0300

    12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 12. Log stats """
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection):
+    """ provides some stats about Nginx logs stored in MongoDB """
+    print(f"{mongo_collection.estimated_document_count()} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {mongo_collection.count_documents({
+        'method': 'GET'})}")
+    print(f"\tmethod POST: {mongo_collection.count_documents({
+        'method': 'POST'})}")
+    print(f"\tmethod PUT: {mongo_collection.count_documents({
+        'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {mongo_collection.count_documents({
+        'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {mongo_collection.count_documents({
+        'method': 'DELETE'})}")
+    print(f"{mongo_collection.count_documents({
+        'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    log_stats(logs)
